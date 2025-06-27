24ebd253a26334efa3d65b39e2356c94728db1ec
Wuttha <Wuttha@webmail.npru.ac.th>
2025-06-27 16:07:58 +0700

    first time

---
+++ error/TestError.java
@@ -0,0 +1,11 @@
+public class TestError {
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+
+        // 2. Runtime error
+        System.out.println(5);
+
+        // 3. Logical Error
+        System.out.println(4.0/10); 
+    }
+}
